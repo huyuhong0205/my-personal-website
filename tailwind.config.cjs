53602caa678cd2e0c4df250ff5f036b15f7e0d4f
yuhong <huyuhong0205@gmail.com>
2023-08-03 15:39:47 +0800

    feat(styles): config tailwind css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      fontFamily: {
+        primary:
+          " 'm plus rounded 1c', 'noto sans tc', 'Verdana', 'sans-serif' ",
+        code: " Menlo, Consolas, 'IBM Plex Mono', monospace ",
+      },
+    },
+  },
+  plugins: [],
+};
