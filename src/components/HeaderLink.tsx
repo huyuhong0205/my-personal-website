15d9101686d19d79da2eb00c160ebfd85077c15d
yuhong <huyuhong0205@gmail.com>
2023-08-07 14:45:57 +0800

    fix(styles): hover to group hover on header link

--- src/components/HeaderLink.tsx
+++ src/components/HeaderLink.tsx
@@ -17,10 +17,10 @@ export default function HeaderLink({ href, title, Icon }: Props) {
       <a
         href={href}
         target="_blank"
-        className="flex items-center gap-1 p-2 text-md"
+        className="group flex items-center gap-1 p-2 text-md"
       >
         <Icon className="h-5 w-5 md:h-4 md:w-4 text-neutral-900" />
-        <span className="hidden md:inline-block hover:underline hover:underline-offset-2 hover:decoration-neutral-900">
+        <span className="hidden md:inline-block group-hover:underline group-hover:underline-offset-2 group-hover:decoration-neutral-900">
           {title}
         </span>
       </a>
