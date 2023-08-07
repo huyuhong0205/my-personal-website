32f4972780e07faa0d440d30ce78b4db34a86763
yuhong <huyuhong0205@gmail.com>
2023-08-07 14:48:58 +0800

    fix(styles): change section title font family

--- src/components/main/SectionTitle.tsx
+++ src/components/main/SectionTitle.tsx
@@ -10,7 +10,7 @@ export default function SectionTitle({ children }: Props) {
   /* JSX ---------------------------------------------------------- */
   return (
     <div className="pb-6">
-      <h2 className="inline text-2xl font-medium tracking-wider border-b-[3px] border-neutral-400">
+      <h2 className="inline font-title text-2xl font-medium tracking-wide border-b-[3px] border-neutral-400">
         {children}
       </h2>
     </div>
