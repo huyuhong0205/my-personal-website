6fb46bb7dec197010dfb3c36cc2023c5a696208a
yuhong <huyuhong0205@gmail.com>
2023-08-08 20:45:39 +0800

    fix(data): modify skills data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -12,18 +12,30 @@ export const skills: Skill[] = [
       { skillTitle: 'HTML', skillIcon: 'html' },
       { skillTitle: 'CSS', skillIcon: 'css' },
     ],
-    descriptions: ['description test test', 'description test test'],
+    descriptions: [
+      'Familiar with HTML5 semantic tags.',
+      'Using Flex box and Grid to achieve flexible page layout and alignment.',
+      'Utilize media queries and flexible layouts to achieve RWD across various screen sizes.',
+    ],
   },
   {
-    titles: [{ skillTitle: 'Javascript', skillIcon: 'javascript' }],
+    titles: [
+      { skillTitle: 'Javascript', skillIcon: 'javascript' },
+      { skillTitle: 'Typescript', skillIcon: 'typescript' },
+    ],
     descriptions: [
-      'description test test description test test description test test description test test',
-      'description test test',
+      'Familiar with the basics of JavaScript and the new features of ES6.',
+      'Using the Fetch API to make asynchronous requests for handling API calls.',
+      "Familiar with using TypeScript's types and generics.",
+      'Experienced in using TypeScript with React, Node.js, and various libraries.',
     ],
   },
   {
     titles: [{ skillTitle: 'React, React Native', skillIcon: 'react' }],
-    descriptions: ['description test test', 'description test test'],
+    descriptions: [
+      "Familiar with React's functional components and common hooks.",
+      'Experienced in using common React libraries such as React Redux, Redux Toolkit, and React Router Dom.',
+    ],
   },
   {
     titles: [
@@ -31,12 +43,8 @@ export const skills: Skill[] = [
       { skillTitle: 'MongoDB', skillIcon: 'mongodb' },
     ],
     descriptions: [
-      'description test test description test test',
-      'description test test',
+      'Understanding the principles and process of communication between the front end and back end.',
+      'Using Node.js, Express, and MongoDB to build backend RESTful APIs and communicate with the frontend.',
     ],
-  },
-  {
-    titles: [{ skillTitle: 'Typescript', skillIcon: 'typescript' }],
-    descriptions: ['description test test', 'description test test'],
   },
 ];
