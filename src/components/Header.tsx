a4ce07722b333061df1051b17a29b7c1befbf6a6
yuhong <huyuhong0205@gmail.com>
2023-08-21 10:43:00 +0800

    fix(header): increase header z index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ import HeaderLink from './HeaderLink';
 export default function Header() {
   /* JSX ---------------------------------------------------------- */
   return (
-    <header className="fixed h-16 w-full px-3 sm:px-5 border-b border-zinc-900/10 bg-white/20 backdrop-blur-md">
+    <header className="z-10 fixed h-16 w-full px-3 sm:px-5 border-b border-zinc-900/10 bg-white/20 backdrop-blur-md">
       <div className="h-full max-w-4xl md:px-5 mx-auto flex justify-between items-center">
         <a href="/" className="font-title font-bold text-lg">
           YU-HONG HU
